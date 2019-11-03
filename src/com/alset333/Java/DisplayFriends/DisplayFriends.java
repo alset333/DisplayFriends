5486a6e9af92605ddc2c98bbd235d091ef69f287
Peter Maar <alset333@icloud.com>
2019-11-02 23:11:46 -0400

    Hello, World!

---
+++ src/com/alset333/Java/DisplayFriends/DisplayFriends.java
@@ -0,0 +1,14 @@
+package com.alset333.Java.DisplayFriends;
+
+public class DisplayFriends {
+
+	public DisplayFriends() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello, world!");
+
+	}
+
+}
